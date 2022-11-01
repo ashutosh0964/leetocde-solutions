ce5ca4c463b3ee781a23db589e125266fc432165
ashutosh0964 <98749945+ashutosh0964@users.noreply.github.com>
2022-11-01 22:31:51 +0530

    Time: 290 ms (5.02%), Space: 109.6 MB (5.36%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -4,11 +4,11 @@ public:
     int c1 = INT_MAX, c2 = INT_MAX;
     for (int x : nums) {
         if (x <= c1) {
-            c1 = x;           // c1 is min seen so far (it's a candidate for 1st element)
-        } else if (x <= c2) { // here when x > c1, i.e. x might be either c2 or c3
-            c2 = x;           // x is better than the current c2, store it
-        } else {              // here when we have/had c1 < c2 already and x > c2
-            return true;      // the increasing subsequence of 3 elements exists
+            c1 = x;           
+        } else if (x <= c2) { 
+            c2 = x;           
+        } else {              
+            return true;      
         }
     }
     return false;
